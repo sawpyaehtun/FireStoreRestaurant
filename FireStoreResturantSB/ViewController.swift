93f093ae3d640dc6ee4c11b54d1e4f2b2b715974
sawpyae <sawpyae.htun@haulio.io>
2019-10-23 10:31:28 +0630

    Initial Commit

---
+++ FireStoreResturantSB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireStoreResturantSB
+//
+//  Created by saw pyaehtun on 23/10/2019.
+//  Copyright © 2019 saw pyaehtun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
